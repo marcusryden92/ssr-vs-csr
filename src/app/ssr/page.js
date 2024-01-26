8135569ccc1151f1d557ee3f513365b9415bf646
marcusryden92 <marcus.ryden1992@gmail.com>
2024-01-26 12:37:44 +0100

    done

--- src/app/ssr/page.js
+++ src/app/ssr/page.js
@@ -6,13 +6,24 @@ import { imageUrlToBase64 } from "@/helpers";
 
 async function getData() {
   // Exercise 1 - Fetch the data here...
-  //
-  //
+
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=1&thumbs=true`,
+    { cache: "no-cache" }
+  );
+
+  if (!response.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  const [json] = await response.json();
+
   // Exercise 2 - Optimize the image loading here...
-  //
-  //
-  // Uncomment this when you're ready
-  // return json
+
+  json.image = await imageUrlToBase64(json.url);
+
+  return json;
 }
 
 export default async function SSR() {
